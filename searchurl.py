e4195a8ae3d32a18f84d2f8d93987e855c6783a2
chrisbog <cbogdon@cisco.com>
2016-07-13 16:28:25 -0400

    Provided code modifications to support Python 3.x instead of 2.x.

--- searchurl.py
+++ searchurl.py
@@ -8,23 +8,25 @@
 #
 
 
-import sys   
+import sys
 
+print("\n\n\nString Parser")
+print("-------------")
 
-print "\n\n\nString Parser"
-print "-------------"
+if sys.version_info < (3, 0):
+    print("Must use python 3.0 or greater\n")
+    exit()
 
 if len(sys.argv) < 2:
-	
-	print "Not enough paramters passed to the application! \n"
-	print sys.argv[0] + " {search_string} {filename}\n" 
-	exit()
+    print("Not enough paramters passed to the application! \n")
+    print(sys.argv[0] + " {search_string} {filename}\n")
+    exit()
 
-print "Search String: " + sys.argv[1]
-print "Parsing file: " + sys.argv[2]
+print("Search String: " + sys.argv[1])
+print("Parsing file: " + sys.argv[2])
 
 # Open the file that is parsed to the application
-                                       
+
 f = open(sys.argv[2])
 
 filetext = f.read()
@@ -35,41 +37,36 @@ filetext = f.read()
 searchstring = sys.argv[1]
 count = 0
 
-print "\n\nSearching the file for all occurences of: %s\n\n" % searchstring
+print("\n\nSearching the file for all occurences of: %s\n\n" % searchstring)
 
-print '{0:40} {1}'.format('Link Title on Page','URL')
-print '{0:40} {1}'.format('=======================================','==========================================')
+print('{0:40} {1}'.format('Link Title on Page', 'URL'))
+print('{0:40} {1}'.format('=======================================', '=========================================='))
 
 # For each line in the file, let's start parsing and searching
 for item in filetext.split("\n"):
 
-	# If the searchstring is found, then let's try to parse out the fields
-	if searchstring in item:
-		
-		# First save the original line
-		line = item.strip()
-		
-		# Parse thru the line to determine the appropriate href location
-		loc =  line.find ("href=")
-		loc = loc+6
-		substring = line[loc:]
-		endloc = substring.find ("\"")
+    # If the searchstring is found, then let's try to parse out the fields
+    if searchstring in item:
+        # First save the original line
+        line = item.strip()
 
-		# Parse through to determine the name
-		start_bracket_loc = substring.find (">")+1
-		end_bracket_loc = substring.find ("<")
-		
-		link_name = substring[start_bracket_loc:end_bracket_loc]
-		
-		# Print the resulting line
-#		print link_name + ":  " + substring[:endloc]
-		print '{0:40} {1}'.format(link_name,substring[:endloc])
-		count = count+1
-		
-print "\n\n"
-print "Total number of occurences of count: %d" % count
-print "\n\n"
+        # Parse thru the line to determine the appropriate href location
+        loc = line.find("href=")
+        loc = loc + 6
+        substring = line[loc:]
+        endloc = substring.find("\"")
 
+        # Parse through to determine the name
+        start_bracket_loc = substring.find(">") + 1
+        end_bracket_loc = substring.find("<")
 
+        link_name = substring[start_bracket_loc:end_bracket_loc]
 
- 
+        # Print the resulting line
+        #		print link_name + ":  " + substring[:endloc]
+        print('{0:40} {1}'.format(link_name, substring[:endloc]))
+        count = count + 1
+
+print("\n\n")
+print("Total number of occurences of count: %d" % count)
+print("\n\n")
